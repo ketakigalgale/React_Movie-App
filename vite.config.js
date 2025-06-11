dab7137853d364fa6b56b12ccdd06551ff28ccb4
ketakigalgale <ketakigalgale19@gmail.com>
2025-06-11 17:18:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/React_Movie-App/", // use your GitHub repo name here
+  base: "/React_Movie-App/",
   plugins: [react()],
 })
